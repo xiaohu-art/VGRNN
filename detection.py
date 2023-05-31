bugfix for sigmoid and kl divergence

detection.py
@@ -9,7 +9,7 @@ import hydra
 import time
 import logging
 
-from utils import loader, mask_edges_det, get_roc_scores, visualize
+from utils import loader, mask_edges_det, get_roc_scores, visualize, set_seed_everywhere
 from model import VGRNN
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 @hydra.main(config_path="cfgs", config_name="config", version_base="1.3")
 def main(cfg):
     seed = cfg.seed
-    np.random.seed(seed)
+    set_seed_everywhere(seed)
 
     '''
     loading data
@@ -128,11 +128,15 @@ def main(cfg):
             auc_val.append(np.mean(np.array(auc_scores_det_val)))
             ap_val.append(np.mean(np.array(ap_scores_det_val)))
             auc_test.append(np.mean(np.array(auc_scores_det_test)))
-            ap_val.append(np.mean(np.array(ap_scores_det_tes)))
+            ap_test.append(np.mean(np.array(ap_scores_det_tes)))
 
-            visualize(kld_losses, nll_losses, losses, auc_val, ap_val, auc_test, ap_test)
-            logger.info(f"Epoch: {k}, kld loss: {kld_loss.item()}, nll loss: {nll_loss.item()}, loss: {loss.item()}")
-    
+            visualize(kld_losses, nll_losses, losses, auc_val, ap_val, auc_test, ap_test, cfg)
+            logger.info(f"Epoch: {k}, kld loss: {kld_loss.item()}, nll loss: {nll_loss.item()}, loss: {loss.item()}, time: {time.time() - start_time}s")
+
+    logger.info(f"Best auc val: {np.max(np.array(auc_val))}")
+    logger.info(f"Best ap val: {np.max(np.array(ap_val))}")
+    logger.info(f"Best auc test: {np.max(np.array(auc_test))}")
+    logger.info(f"Best ap test: {np.max(np.array(ap_test))}")   
 
 if __name__ == "__main__":
     main()

model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, SAGEConv, GINConv
 
 class InnerProductDecoder(nn.Module):
     def __init__(self, act=torch.sigmoid, dropout=0.):
@@ -69,6 +69,58 @@ class graph_gru_gcn(nn.Module):
         
         out = h_out
         return out, h_out
+    
+class graph_gru_sage(nn.Module):
+    def __init__(self, input_size, hidden_size, n_layer, bias=True, device="cuda"):
+        super(graph_gru_sage, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.n_layer = n_layer
+        
+        self.device = device
+
+        # gru weights
+        self.weight_xz = []
+        self.weight_hz = []
+        self.weight_xr = []
+        self.weight_hr = []
+        self.weight_xh = []
+        self.weight_hh = []
+        
+        for i in range(self.n_layer):
+            if i==0:
+                self.weight_xz.append(SAGEConv(input_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+                self.weight_hz.append(SAGEConv(hidden_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+                self.weight_xr.append(SAGEConv(input_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+                self.weight_hr.append(SAGEConv(hidden_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+                self.weight_xh.append(SAGEConv(input_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+                self.weight_hh.append(SAGEConv(hidden_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+            else:
+                self.weight_xz.append(SAGEConv(hidden_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+                self.weight_hz.append(SAGEConv(hidden_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+                self.weight_xr.append(SAGEConv(hidden_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+                self.weight_hr.append(SAGEConv(hidden_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+                self.weight_xh.append(SAGEConv(hidden_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+                self.weight_hh.append(SAGEConv(hidden_size, hidden_size, act=lambda x:x, bias=bias).to(device))
+    
+    def forward(self, inp, edgidx, h):
+        h_out = torch.zeros(h.size()).to(self.device)
+        for i in range(self.n_layer):
+            if i==0:
+                z_g = torch.sigmoid(self.weight_xz[i](inp, edgidx) + self.weight_hz[i](h[i], edgidx))
+                r_g = torch.sigmoid(self.weight_xr[i](inp, edgidx) + self.weight_hr[i](h[i], edgidx))
+                h_tilde_g = torch.tanh(self.weight_xh[i](inp, edgidx) + self.weight_hh[i](r_g * h[i], edgidx))
+                
+            else:
+                z_g = torch.sigmoid(self.weight_xz[i](h_out[i-1], edgidx) + self.weight_hz[i](h[i], edgidx))
+                r_g = torch.sigmoid(self.weight_xr[i](h_out[i-1], edgidx) + self.weight_hr[i](h[i], edgidx))
+                h_tilde_g = torch.tanh(self.weight_xh[i](h_out[i-1], edgidx) + self.weight_hh[i](r_g * h[i], edgidx))
+
+            h_out[i] = z_g * h[i] + (1 - z_g) * h_tilde_g
+
+        
+        out = h_out
+        return out, h_out
 
 class VGRNN(nn.Module):
     def __init__(self, x_dim, h_dim, z_dim, n_layers, eps, conv='GCN', bias=False, device="cuda"):
@@ -87,8 +139,8 @@ class VGRNN(nn.Module):
             self.phi_z = nn.Sequential(nn.Linear(z_dim, h_dim), nn.ReLU()).to(device)
 
             self.enc = GCNConv(h_dim + h_dim, h_dim).to(device)
-            self.enc_mean = GCNConv(h_dim, z_dim, act=lambda x:x).to(device)
-            self.enc_std = GCNConv(h_dim, z_dim, act=F.softplus).to(device)
+            self.enc_mean = GCNConv(h_dim, z_dim).to(device)
+            self.enc_std = GCNConv(h_dim, z_dim).to(device)
 
             self.prior = nn.Sequential(nn.Linear(h_dim, h_dim), nn.ReLU()).to(device)
             self.prior_mean = nn.Sequential(nn.Linear(h_dim, z_dim)).to(device)
@@ -96,8 +148,19 @@ class VGRNN(nn.Module):
 
             self.rnn = graph_gru_gcn(h_dim + h_dim, h_dim, n_layers, bias, device).to(device)
 
-        else:
-            raise NotImplementedError
+        elif conv == "SAGE":
+            self.phi_x = nn.Sequential(nn.Linear(x_dim, h_dim), nn.ReLU()).to(device)
+            self.phi_z = nn.Sequential(nn.Linear(z_dim, h_dim), nn.ReLU()).to(device)
+            
+            self.enc = SAGEConv(h_dim + h_dim, h_dim).to(device)
+            self.enc_mean = SAGEConv(h_dim, z_dim, act=lambda x:x).to(device)
+            self.enc_std = SAGEConv(h_dim, z_dim, act=F.softplus).to(device)
+            
+            self.prior = nn.Sequential(nn.Linear(h_dim, h_dim), nn.ReLU()).to(device)
+            self.prior_mean = nn.Sequential(nn.Linear(h_dim, z_dim)).to(device)
+            self.prior_std = nn.Sequential(nn.Linear(h_dim, z_dim), nn.Softplus()).to(device)
+
+            self.rnn = graph_gru_sage(h_dim + h_dim, h_dim, n_layers, bias).to(device)
         
         # self.decoder = InnerProductDecoder(act=lambda x: x).to(device)
         self.decoder = InnerProductDecoder().to(device)
@@ -122,6 +185,7 @@ class VGRNN(nn.Module):
             enc_t = self.enc(torch.cat([phi_x_t, h[-1]], dim=1), edge_idx_list[t]).to(self.device)
             enc_mean_t = self.enc_mean(enc_t, edge_idx_list[t]).to(self.device)
             enc_std_t = self.enc_std(enc_t, edge_idx_list[t]).to(self.device)
+            enc_std_t = nn.Softplus()(enc_std_t).to(self.device)
 
             # Prior
             prior_t = self.prior(h[-1]).to(self.device)
@@ -172,16 +236,9 @@ class VGRNN(nn.Module):
 
     def _kld_gauss(self, mean_1, std_1, mean_2, std_2):
         num_nodes = mean_1.size()[0]
-        kld_element =  (2 * torch.log(std_2 + self.eps) - 2 * torch.log(std_1 + self.eps) -
-                        (torch.pow(std_1 + self.eps ,2) + torch.pow(mean_1 - mean_2, 2)) / 
-                        torch.pow(std_2 + self.eps ,2) + 1)
-        return (0.5 / num_nodes) * torch.mean(torch.sum(kld_element, dim=0), dim=0)
-    
-    def _kld_gauss_bak(self, mean_1, std_1, mean_2, std_2):
-        num_nodes = mean_1.size()[0]
         kld_element =  (2 * torch.log(std_2 + self.eps) - 2 * torch.log(std_1 + self.eps) +
-                        (torch.pow(std_1 + self.eps ,2) + torch.pow(mean_1 - mean_2, 2)) / 
-                        torch.pow(std_2 + self.eps ,2) - 1)
+                        (torch.pow(std_1 + self.eps ,2) + torch.pow(mean_1 - mean_2, 2)) / torch.pow(std_2 + self.eps ,2)
+                        - 1)
         return (0.5 / num_nodes) * torch.mean(torch.sum(kld_element, dim=1), dim=0)
     
     def _nll_bernoulli(self, logits, target_adj_dense):

utils.py
@@ -1,9 +1,20 @@
 import pickle
 import numpy as np
 import scipy.sparse as sp
+import random
+import os
 from sklearn.metrics import roc_auc_score, average_precision_score
 import torch
 import matplotlib.pyplot as plt
+
+def set_seed_everywhere(seed=0):
+    """
+    Set seed for all randomness sources
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
 
 def loader(datasets):
 
@@ -120,7 +131,10 @@ def mask_edges_det(adjs_list):
 @torch.no_grad()
 def get_roc_scores(edges_pos, edges_neg, adj_orig_dense_list, embs):
     def sigmoid(x):
-        return 1 / (1 + np.exp(-x))
+        if x < 0:
+            return np.exp(x) / (1+np.exp(x))
+        else:
+            return 1 / (1 + np.exp(-x))
     
     auc_scores = []
     ap_scores = []
@@ -133,7 +147,7 @@ def get_roc_scores(edges_pos, edges_neg, adj_orig_dense_list, embs):
         preds = []
         pos = []
         for e in edges_pos[i]:
-            preds.append(sigmoid(adj_rec[e[0], e[1]]))
+            preds.append(adj_rec[e[0], e[1]])
             pos.append(adj_orig_t[e[0], e[1]])
             
         preds_neg = []
@@ -149,7 +163,7 @@ def get_roc_scores(edges_pos, edges_neg, adj_orig_dense_list, embs):
 
     return auc_scores, ap_scores
 
-def visualize(kld_losses, nll_losses, losses, auc_val, ap_val, auc_test, ap_test):
+def visualize(kld_losses, nll_losses, losses, auc_val, ap_val, auc_test, ap_test, cfg):
     
     plt.figure(figsize=(20, 6))
 
@@ -170,5 +184,5 @@ def visualize(kld_losses, nll_losses, losses, auc_val, ap_val, auc_test, ap_test
     plt.legend()
     plt.xlabel("epoch / interval")
 
-    plt.savefig("result.png")
+    plt.savefig(f"{cfg.conv_type}_{cfg.datasets}.png")
     plt.close()